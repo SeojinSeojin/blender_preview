fcd0b9562bc201de507473b5f63f67d5b8d365ed
SeojinSeojin <1106laura@naver.com>
2021-03-02 17:34:50 +0900

    [Feat] Animation Fix, [Feat] Add Custom Patt

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 document.addEventListener("click", () => {
     const box_asset = document.querySelector("#box-asset");
+    if (box_asset.hasAttribute("animation-mixer")) {
+        box_asset.removeAttribute("animation-mixer");
+    }
     box_asset.setAttribute("animation-mixer", "loop:once; clampWhenFinished:true");
 })
